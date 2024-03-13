7c1f70016a4a245eea6d70ef1f1582f7c202e732
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-14 00:34:42 +0300

    revize

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 
+import QRCode from "react-qr-code";
+
+
 function App() {
   return (
-    <div >
-      header
+    <div>
+    <header>
+      <h1>
+       😎 MEHMET DOĞAN
+      </h1>
+      <QRCode 
+      size={256}
+      style={{height:"auto",maxWidth:"200px",width:"200px"}}
+      />
+    </header>
     </div>
   );
 }
